ed952702ebe5a6d8980422bc5e674e962abfe7d7
Mate Steinforth <mate1@gmx.net>
2019-11-11 20:02:25 +0100

    Update arduino_padded_zeros1.ino
    added optional parameter passing method

--- arduino_padded_zeros1.ino
+++ arduino_padded_zeros1.ino
@@ -3,23 +3,32 @@
 // (c) matesteinforth.com 2019
 //
 
-char buf[10]; // define global buffer with 9 digits space (long)
-
 void setup() {  
-  	Serial.begin(9600); 
+  	Serial.begin(9600); // open serial port for debug
+
+  	char myBuf[10];
+	padd(myBuf, 3, 9);
+	Serial.println(myBuf);
 }
 
 void loop() {
-    Serial.println(paddd(123456, 9));
 }
 
-char * paddd (long number, int digits)
+// as a return function, use with global buffer
+char * paddd (long number, int digits) 
 {	  
-  int i=digits;
-  while (i > 0) {
-    buf[i-1]=(number%10)+48; // converts number to ASCII, writes it in Char Array
-    number = number / 10;
-    i--;    
+  	for (int i = digits; i >0; i--) { // loop over decimals
+      	buf[i-1]=(number%10)+48; // converts number to ASCII, writes it in Char Array
+    	number /= 10; // next decimal
  	}
-  return buf;
+   	return buf;
+}
+
+// pass a buffer to function and read it's content, see above
+void padd (char *buf, long number, int digits) 
+{	  
+  	for (int i = digits; i >0; i--) { // loop over decimals
+      	buf[i-1]=(number%10)+48; // converts number to ASCII, writes it in Char Array
+    	number /= 10; // next decimal
+ 	}
 }
