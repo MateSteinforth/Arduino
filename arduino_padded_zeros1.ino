d950f8ebf376b76ebeb331e903819f0520c57911
Mate Steinforth <mate1@gmx.net>
2019-11-11 15:38:24 +0100

    Update arduino_padded_zeros1.ino
    added comments

--- arduino_padded_zeros1.ino
+++ arduino_padded_zeros1.ino
@@ -3,23 +3,23 @@
 // (c) matesteinforth.com 2019
 //
 
-char buf[10];
+char buf[10]; // define global buffer with 9 digits space (long)
 
 void setup() {  
   	Serial.begin(9600); 
-    Serial.println(paddd(123456, 9));
 }
 
 void loop() {
+    Serial.println(paddd(123456, 9));
 }
 
 char * paddd (long number, int digits)
 {	  
-  	int i=digits;
-  	while (i > 0) {
-      	buf[i-1]=(number%10)+48;
-    	number = number / 10;
-    	i--;    
+  int i=digits;
+  while (i > 0) {
+    buf[i-1]=(number%10)+48; // converts number to ASCII, writes it in Char Array
+    number = number / 10;
+    i--;    
  	}
-   	return buf;
+  return buf;
 }
